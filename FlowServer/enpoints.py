83eea209a82cb6aef468ecf0415060ad3986be94
hauminix <tim.matteo.brunner@gmail.com>
2022-01-19 13:00:45 +0100

    json definition

--- FlowServer/enpoints.py
+++ FlowServer/enpoints.py
@@ -4,7 +4,11 @@ import requests
 from .src import LineDetection
 from .src import Latex
 
-
+#<-- Test data
+NeuralTestJson = '[{"component":"R","position":{"X":130,"Y":120},"pins":[{"X":120,"Y":120},{"X":140,"Y":120}]},{"component":"C","position":{"X":160,"Y":120},"pins":[{"X":150,"Y":120},{"X":170,"Y":120}]}]'
+NetTestJson = '[{"component":"R","position":{"X":130,"Y":120},"pins":[{"X":120,"Y":120,"ID":1},{"X":140,"Y":120,"ID":2}]},{"component":"C","position":{"X":160,"Y":120},"pins":[{"X":150,"Y":120,"ID":1},{"X":170,"Y":120,"ID":2}]}]'
+LineTestJson = ''
+#--> To Remove
 class Neural:
     def __init__(self):
         self._test = ""
@@ -12,11 +16,17 @@ class Neural:
         data = json.loads(await req.stream.read())
         outputMethodes = data["outputs"]
         imageBase64 = data["image"]
-        neuralOutput = None#requests.post('http://localhost:8501/v1/models/nn:predict', data = json.dumps({"image": imageBase64}))
-        netList = LineDetection.detect(neuralOutput)
 
+        #Send picture data to TensorflowServing for evaluation
+        neuralOutput = requests.post('http://localhost:8501/v1/models/nn:predict', data = json.dumps({"image": imageBase64}))
+
+        #Use linedetection to generate netlist and linelist
+        netList,lineList = LineDetection.detect(neuralOutput, imageBase64)
+
+        #Build output json
         output = "{"
         convert = [False,False]
+        #Check which formats were chossen
         for i in range(0, len(outputMethodes)):
             if(outputMethodes[i] == "netlist"):
                 convert[0] = True
@@ -24,12 +34,14 @@ class Neural:
                 convert[1] = True
         
         if(convert[0]):
+            #If chossen add netlist to response
             output += "netlist:" + netList
             if(convert[1]): output += ","
         if(convert[1]):
-            netList = data["testnet"]
-            output += '"latex:"' + Latex.ListToLatex(netList)
+            #If chossen add Latex code to response
+            output += '"latex:"' + Latex.ListToLatex(netList,lineList)
         output += "}"
 
-        resp.media = output#json.loads(output)
+        #Send back requested data
+        resp.media = json.loads(output)
         resp.status = 200
