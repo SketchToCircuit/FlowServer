0601bc9e25a77582cb3a49aedf8c0ff6ecafd394
SaltySebi <53235949+SaltySebi@users.noreply.github.com>
2022-02-15 22:36:08 +0100

    pain... straight pain

--- FlowServer/src/LineDetection.py
+++ FlowServer/src/LineDetection.py
@@ -1,4 +1,5 @@
 from base64 import b64decode
+from pickle import FALSE, TRUE
 import sys
 import math
 import cv2 as cv
@@ -6,24 +7,92 @@ import numpy as np
 import json
 from jsonc_parser.parser import JsoncParser
 
-#Workflow detect net
-#start at begining of the components
-#go to first endpoint compare endpoint to nearest point and make the conection
-#then go to the endpoint of the conected line and repeat as long as you end with a Circuit Copmonents pin then go to an usatisfie conection 
-#whilst doing that if there are more than one hit(a junction) in the radius set them as "unsatisfied pins"
-#alle lines in der nähe von von components die nicht in der richtung des components wegehen
-#werden geditched (diese überprüfung nur dan wenn sie schon in der bounding + 10% boxliegen)
-#the function ends if all lines are satisfied
-#a line is setisfied when it got acknoledgd as useles or none of its pins/conected lines are unsatisfied
-
-#gepfuschte static vars fürs erste
-satisfied = 0
-
 def detectNet(lines, neuralOut):
+    #lines left defines how many lines(pointsare left)
+    HITRADIUS = 40 #px
+    linesLeft = lines
+    nets = []
 
-    if (satisfied == len(lines)):
-        #export the nets to json
-        return None
+    #create a list of all component pins
+    pinPoints = []
+    for pins in neuralOut["pins"]:
+        for pin in pins:
+            pinPoints.append((pin["x"], pin["y"]))
+
+    #creates a list double in length but identical in order to lines but as individual points (x, y)
+    linePoints = []
+    for line in lines:
+        linePoints.append((line[0], line[1]))
+        linePoints.append((line[2], line[3]))
+
+    currPoint = linePoints[1]
+    unsatisfied = []
+    unsatisfied.append(0)
+
+    while linesLeft:
+        net = []
+        #if the direction of a line diverts by a certein threshhold of the line befor it gets counted as imtersection and is counted as Unsatisfied
+        netIsFinished = FALSE
+        currPoint = linesLeft[1]
+        unsatisfied = []
+        conected = []
+
+        while not netIsFinished:
+            
+            #check for component pins if they touch and if they do thier id will be the iterator of thier list + length of linePoints
+            for i in range(len(pinPoints)):
+                distance = math.sqrt((currPoint(2) -pinPoints[i][0])**2 + (currPoint(3) -pinPoints[i][0])**2)
+                if distance <= HITRADIUS:
+                    conected.append(i + len(pinPoints))
+
+            #check which lines "touch" each other           
+            for i in range(len(linePoints)):
+                distance = math.sqrt((currPoint(2) -linePoints[i][0])**2 + (currPoint(3) -linePoints[i][0])**2)
+                if distance <= HITRADIUS:
+                    conected.append(i)
+
+            #if there is no point around
+            if len(conected) == 0:
+                if len(unsatisfied) == 0:
+                    netIsFinished = TRUE
+                    #break
+
+                net.append(
+                    (currPoint),
+                    []
+                )
+            else:   
+                #fill in the detected point into the net
+                net.append(
+                    (currPoint),
+                    conected
+                )
+                       
+
+            #ToDo if the line hits only the component it will fail
+            #set the firstly detected points endpoint as new current Point
+            if conected[0] > len(pinPoints):
+                #if first conected was a pinPoint set an unsatisfied
+                 if conected[0] % 2 == 0:
+                    currPoint = unsatisfied[0] + 1
+                    del unsatisfied[0]
+                 else:
+                    currPoint = unsatisfied[0] - 1
+                    del unsatisfied[0] 
+            elif conected[0] % 2 == 0:
+                currPoint = conected[0] + 1
+            else:
+                currPoint = conected[0] - 1
+            #add all conections which arent traced yet to the unsatisfied conections
+            unsatisfied.extend(conected[1:])
+
+        #put he dcetected net into the nets      
+        nets.append(net)
+
+    return 0
+
+
+    
     
 def NetListExP(neuralOut):
     NetList = []
@@ -46,6 +115,7 @@ def NetListExP(neuralOut):
         NetList.append({
             "component": comp["component"],
             "position": { 
+                #center the component
                 "x": topleft[0] + (abs(topleft[0] - botright[0]) / 2),
                 "y": topleft[1] + (abs(topleft[1] - botright[1]) / 2)},
             "pins": pins
@@ -57,13 +127,55 @@ def detect(img, neuralOut):
     cdstP = np.copy(cv.cvtColor(img, cv.COLOR_GRAY2BGR))
     _, img = cv.threshold(img,128, 255, cv.THRESH_BINARY_INV)
 
-    #kernel = np.ones((5,5),np.uint8)
-    #img = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
+    
     _, img = cv.threshold(img,128, 255, cv.THRESH_BINARY)
     rows = img.shape[0]
 
-    linesP = cv.HoughLinesP(img, 1, np.pi / 180, 50, 10, 10)
+    #for now lines will be preped by whiteboxing all components
+    for component in neuralOut:
+        #set the width of the boundingbox to the points
+        newTopleft = (
+            component["topleft"]["x"],
+            component["topleft"]["y"]         
+        )
+        newBotright = (
+            component["bottomright"]["x"],
+            component["bottomright"]["y"]
+        )
+        
+        for pins in component["pins"]:
+            newTopleft = (
+                min(newTopleft[0], pins["x"]),
+                component["topleft"]["y"]
+            )
+            newBotright = (
+                max(newBotright[0], pins["x"]),
+                component["bottomright"]["y"]
+            )
+        #reduce the height of the bounding box by 10%
+        newTopleft = (
+            int(newTopleft[0] * 1.05),
+            int(newTopleft[1] * 1.05)
+        )
 
+        newBotright = (
+            int(newBotright[0] * 0.95),
+            int(newBotright[1] * 0.95)
+        )
+
+        cv.rectangle(img, newTopleft, newBotright, (0,0,0), -1)
+        cv.rectangle(cdstP, newTopleft, newBotright, (0,0,255), 2)
+
+    #cleaning away little lines after croping components
+    kernel = np.ones((5,5),np.uint8)
+    img = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)   
+
+    #predicting lines 
+    linesP = cv.HoughLinesP(img, 1, np.pi / 180, 25, 5, 5)
+
+
+
+    #draw predicted lines just for debuging
     if linesP is not None:
         for i in range(0, len(linesP)):
             l = linesP[i][0]
@@ -118,16 +230,16 @@ def main():
     neuralOutput = JsoncParser.parse_file('TestData/SampleNeuralOutput.jsonc')
 
     img = cv.imread("TestData/test.jpg", cv.IMREAD_GRAYSCALE)
-    #detect(img, jsonObject)
+    detect(img, neuralOutput)
     
-    netList = NetListExP(neuralOutput)
+    #netList = NetListExP(neuralOutput)
 
-    netList = reorderPins(netList)
+    #netList = reorderPins(netList)
 
-    opv = [cmp for cmp in netList if cmp['component'] == 'OPV']
-    npn = [cmp for cmp in netList if cmp['component'] == 'NPN']
-    print(opv)
-    print(npn)
+    #opv = [cmp for cmp in netList if cmp['component'] == 'OPV']
+    #npn = [cmp for cmp in netList if cmp['component'] == 'NPN']
+    #print(opv)
+    #print(npn)
 
 if __name__ == "__main__":
     main()
